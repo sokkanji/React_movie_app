ec389be32d02e312e66a3368f161e24ce83b651b
sokkanji <sskkanji@gmail.com>
2020-08-19 22:15:52 +0900

    Feat : Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+function Food({ fav }){
+  return <h1>I like { fav }</h1>;
+}
+
 function App() {
   return (
     <div>
       <h1>Hello React!!</h1>
+      <Food fav="kimchi"/>
+      <Food fav="ramen"/>
+      <Food fav="samgiopsal"/>
+      <Food fav="chukumi"/>
     </div>
   ); 
 }
