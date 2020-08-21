0e05b75d856237995f99d7b84d46c4358f964f53
sokkanji <sskkanji@gmail.com>
2020-08-21 10:47:11 +0900

    #1 Feat : map Recap

--- src/App.js
+++ src/App.js
@@ -1,48 +1,55 @@
 import React from 'react';
 
-function Food({ name, picture }){
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture}></img>
-    </div>
-  );
-}
-
 const foodILike = [
   {
+    id : 1,
     name : "kimchi",
     image :
       "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
   },
   {
+    id : 2,
     name : "Samgyeopsal",
     image :
       "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
   },
   {
+    id : 3,
     name : "Bibibap",
     image :
       "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
   },
   {
+    id : 4,
     name : "Doncasu",
     image :
       "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
   },
   {
+    id : 5,
     name : "kimbap",
     image :
       "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
   },
 ];
 
-function App() {
+function Food({ name, picture }){
   return (
     <div>
-      {foodILike.map( dish => (
-        <Food name={dish.name} picture={dish.image} />
-      ))}
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name}></img>
+    </div>
+  );
+}
+
+function renderFood(dish){
+  return <Food key={dish.id} name={dish.name} picture={dish.image}/>
+}
+
+function App() {
+  return (
+    <div> 
+      {foodILike.map(renderFood)}
     </div>
   ); 
 }
