791b21c1b8269bfe2d4bd0eca8ac022c8f4a8b1a
sokkanji <sskkanji@gmail.com>
2020-08-21 10:15:31 +0900

    Feat : Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React from 'react';
 
-function Food({ fav }){
-  return <h1>I like { fav }</h1>;
+function Food({ name, picture }){
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture}></img>
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name : "kimchi",
+    image :
+      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
+  },
+  {
+    name : "Samgyeopsal",
+    image :
+      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
+  },
+  {
+    name : "Bibibap",
+    image :
+      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
+  },
+  {
+    name : "Doncasu",
+    image :
+      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
+  },
+  {
+    name : "kimbap",
+    image :
+      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello React!!</h1>
-      <Food fav="kimchi"/>
-      <Food fav="ramen"/>
-      <Food fav="samgiopsal"/>
-      <Food fav="chukumi"/>
+      {foodILike.map( dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   ); 
 }
