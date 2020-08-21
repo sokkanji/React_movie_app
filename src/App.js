774ffb7e212b8e6e745600b7e9e9b2e3fa0d907f
sokkanji <sskkanji@gmail.com>
2020-08-21 11:49:30 +0900

    #3 Feat : Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,73 +1,23 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id : 1,
-    name : "kimchi",
-    image :
-      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg",
-
-  },
-  {
-    id : 2,
-    name : "Samgyeopsal",
-    image :
-      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg",
-      rating : 4.9
-    },
-  {
-    id : 3,
-    name : "Bibibap",
-    image :
-      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg",
-      rating : 4.7
-  },
-  {
-    id : 4,
-    name : "Doncasu",
-    image :
-      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg",
-      rating : 4.8
-  },
-  {
-    id : 5,
-    name : "kimbap",
-    image :
-      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg",
-      rating : 4.9
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add =() => {
+    console.log("add");
+  };
+  minus =() => {
+    console.log("minus");
+  };
+  render(){
+    return <div>
+      <h1>The number is : {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
   }
-];
-
-function Food({ name, picture, rating }){
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}></img>
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
-
-// function renderFood(dish){
-//   return <Food key={dish.id} name={dish.name} picture={dish.image}/>
-// }
-
-function App() {
-  return (
-    <div> 
-      {        /* {foodILike.map(renderFood)} */ 
-      foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  ); 
 }
 
 export default App;
