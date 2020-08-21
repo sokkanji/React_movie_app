cd675eb5a616ef5a366d92458654220938a57d56
sokkanji <sskkanji@gmail.com>
2020-08-21 11:13:51 +0900

    #2 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,55 +1,71 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
     id : 1,
     name : "kimchi",
     image :
-      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
+      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg",
+
   },
   {
     id : 2,
     name : "Samgyeopsal",
     image :
-      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
-  },
+      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg",
+      rating : 4.9
+    },
   {
     id : 3,
     name : "Bibibap",
     image :
-      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
+      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg",
+      rating : 4.7
   },
   {
     id : 4,
     name : "Doncasu",
     image :
-      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
+      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg",
+      rating : 4.8
   },
   {
     id : 5,
     name : "kimbap",
     image :
-      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg"
-  },
+      "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-seaweed-rice-sushi-roll-illustration-image_1226699.jpg",
+      rating : 4.9
+  }
 ];
 
-function Food({ name, picture }){
+function Food({ name, picture, rating }){
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name}></img>
     </div>
   );
 }
 
-function renderFood(dish){
-  return <Food key={dish.id} name={dish.name} picture={dish.image}/>
-}
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
+
+// function renderFood(dish){
+//   return <Food key={dish.id} name={dish.name} picture={dish.image}/>
+// }
 
 function App() {
   return (
     <div> 
-      {foodILike.map(renderFood)}
+      {        /* {foodILike.map(renderFood)} */ 
+      foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+      ))}
     </div>
   ); 
 }
