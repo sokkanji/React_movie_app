fd1e75815176537daf7f4b281f39f62c9df18fc6
sokkanji <sskkanji@gmail.com>
2020-08-23 02:34:09 +0900

    #15 Feat : Building the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import {HashRouter, Route} from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+
 
 function App(){
-  return <sapn>something</sapn>;
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
