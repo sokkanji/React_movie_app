5f841dbe5bc780836c2d8af28a48c0930fe21242
sokkanji <sskkanji@gmail.com>
2020-08-22 19:13:48 +0900

    #7 Feat : Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import axios from "axios";
 
 class App extends React.Component{
   state = {
-    isLoading: true
-    // movies: []
+    isLoading: true,
+    movies: []
   };
 
+  getMovies = async () => {
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+  }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
 
   render(){
