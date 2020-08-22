2cb57fd3819c748600840e7c6cf06d385b414fa2
sokkanji <sskkanji@gmail.com>
2020-08-23 02:01:44 +0900

    Updates

---
+++ precache-manifest.23940da7b2901fa469895dd63459308f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814ff7a184fcbe0b35eda62faeaba8f3",
+    "url": "/React_movie_app/index.html"
+  },
+  {
+    "revision": "c20653e5ba1398504f16",
+    "url": "/React_movie_app/static/css/main.03a12181.chunk.css"
+  },
+  {
+    "revision": "c75c28e03a147eb36d3f",
+    "url": "/React_movie_app/static/js/2.7b45b7d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_movie_app/static/js/2.7b45b7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c20653e5ba1398504f16",
+    "url": "/React_movie_app/static/js/main.233c9e01.chunk.js"
+  },
+  {
+    "revision": "8ad0589516f27e5ed2d7",
+    "url": "/React_movie_app/static/js/runtime-main.8ec2ad62.js"
+  }
+]);
